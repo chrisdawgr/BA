356471369936b2cab24ee164e1ffa8752060d1fb
slagterfranz <malogest@gmail.com>
2015-09-30 16:25:03 +0200

    Harris updated - Ixx, instead of (Ixx*[-1,0,1])*[-1,0,1]..

--- Harris.py
+++ Harris.py
@@ -6,56 +6,48 @@ def harris(Iname, k, thresh):
     I = cv2.imread(Iname)
     I_bw = cv2.imread(Iname, 0)
     Ixy = numpy.empty([len(I_bw),len(I_bw[0])])
+    Ixx = numpy.empty([len(I_bw),len(I_bw[0])])
+    Iyy = numpy.empty([len(I_bw),len(I_bw[0])])
     C = numpy.empty([len(I_bw),len(I_bw[0])])
 
-	#mask_dx = numpy.array([-1,0,1]
     mask_dx = numpy.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+    #mask_dx = numpy.array([-1,0,1])
     mask_dy = numpy.transpose(mask_dx)
-    mask_dxx = numpy.transpose(mask_dx)
-    mask_dyy = numpy.transpose(mask_dx)
     mask_gauss = numpy.array([[1,4,7,4,1],[4,16,26,16,4],[7,26,41,26,7],[4,16,26,16,4],[1,4,7,4,1]])
     mask_gauss = numpy.divide(mask_gauss, 273.0)
 
-    # Calculate x and y derivatives of the image:
     Ix = cv2.filter2D(I_bw, -1, mask_dx)
     Iy = cv2.filter2D(I_bw, -1, mask_dy)
-    Ixx0 = cv2.filter2D(Ix, -1, mask_dx)
-    Ixx = cv2.filter2D(Ixx0, -1, mask_gauss)
-    Iyy0 = cv2.filter2D(Iy, -1, mask_dy)
-    Iyy = cv2.filter2D(Iyy0, -1, mask_gauss)
-    
-    # Calculate products of the derivatives
+
     for y in range(0, len(I_bw) ):
         for x in range(0, len(I_bw[0])):
             Ixy[y][x] = float(Ix[y][x]) * float(Iy[y][x])
+            Ixx[y][x] = float(Ix[y][x]) * float(Ix[y][x])
+            Iyy[y][x] = float(Iy[y][x]) * float(Iy[y][x])
 
-    # NOTE: contradicting Harris, calculating Det(I(y,x)), as ab-c1c2, instead
-    # of ab-c^2, for possible better results
-    Ixy0 = cv2.filter2D(Ix, -1, mask_dy)
-    Ixy = cv2.filter2D(Ixy0, -1, mask_gauss)
-    Iyx0 = cv2.filter2D(Iy, -1, mask_dx)
-    Iyx = cv2.filter2D(Iyx0, -1, mask_gauss)
 
-    # Calculate R
+    Ixy = cv2.filter2D(Ixy, -1, mask_gauss)
+    Ixx = cv2.filter2D(Ixx, -1, mask_gauss)
+    Iyy = cv2.filter2D(Iyy, -1, mask_gauss)
+
     for y in range(0, len(I_bw) ):
         for x in range(0, len(I_bw[0])):
-            a = float(Ixx[y][x])
-            b = float(Iyy[y][x])
-            c1 = float(Ixy[y][x])
-            c2 = float(Iyx[y][x])
-            C[y][x] = (a*b - c1* c2) - k * (a + b)**2
+            a = Ixx[y][x]
+            b = Iyy[y][x]
+            c = Ixy[y][x]
+            C[y][x] = (a*b - c**2) - k * (a + b)**2
+            #print(C[y][x])
 
-	# Set threshold for R
     for y in range(3, len(C)):
         for x in range(3, len(C[0])):
             if (C[y][x] > thresh):
                 #print(C[y][x])
                 I[y][x] = [0,0,255]
 
-    cv2.imwrite('erimitage-harris.jpg', I)
+    cv2.imwrite('erimitage-harris-cor.jpg', I)
     cv2.imshow('image', I)
     cv2.waitKey(100000)
-harris('erimitage.jpg', 0.04,14920)
+harris('erimitage.jpg', 0.04, 200000000)
 
 def gauss(size, sigma):
     gauss_kernel = numpy.empty([size, size])
