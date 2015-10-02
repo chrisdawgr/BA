4504c06cfe2a733b73f9ec8c98bc9a822a4d6339
slagterfranz <malogest@gmail.com>
2015-10-02 09:36:38 +0200

    updates on orientation-sift. still problems with degrees

--- orientation-sift.py
+++ orientation-sift.py
@@ -1,48 +1,86 @@
-import siftthis as sift
-import harristhis as harris
+import helperfunctions as h
 import cv2
 import math
 import numpy
 
 
+def magnitude(I, point):
+  """
+  Input : Image I, point point
+  Output: outputs the magnitude
+  """
+  w = h.create_window(I, point, 3)
+  mag = math.sqrt((w[1][0] - w[1][2])**2 + (w[0][1] - w[2][1])**2)
+  return(mag)
+
+def orientation(I, point):
+  """
+  Input : Image I, point point
+  Output: orientation of image
+  """
+  w = h.create_window(I, point, 3)
+  Ly = w[0][1] - w[2][1]
+  Lx = w[1][0] - w[1][2]
+  if (Lx != 0):
+    orien = math.atan(float(Ly)/float(Lx))
+    orien = math.degrees(orien)
+    if (orien < 0):
+      return(360 + orien)
+    return(orien)
+  return(0)
+
 def sift_orientation(I, points, window_size):
+  """
+  Input : image I, interest points, size of window
+  Output: assigns an orientation to the interest points
+  """
+
+  # point with orientation; [[y, x],[o1, o2, o3]]
+  pwo = []
+  i = 0
+  max_point_size_y = len(I) - (window_size + 2)
+  min_point_size_y = window_size
+  max_point_size_x = len(I[0]) - (window_size + 5)
+  min_point_size_x = window_size 
+  hold = numpy.zeros([1,36])
+
+  # length of outer list
+  o = 0
   for p in points:
-    y = p[0]
-    x = p[1]
+    if ((p[0] <= max_point_size_y and p[0] >= min_point_size_y)) and \
+        (p[1] <= max_point_size_x and p[1] >= min_point_size_x):
+      o += 1
 
+  # size of all usable points o
+  bins = numpy.zeros([o, 1, 36])
+  gauss_window = h.gauss(window_size + 2, 1.5)
 
+  for p in points:
+    # if a point is too close to the border of the image, it is discarded
+    #print(i)
+    if ((p[0] <= max_point_size_y and p[0] >= min_point_size_y)) and \
+        (p[1] <= max_point_size_x and p[1] >= min_point_size_x):
 
-def create_window(I, point, window_size):
-  D = numpy.empty([window_size, window_size])
-  half_w_size = window_size/2
-  y_p = point[0]
-  x_p = point[1]
+        ip_window = h.create_window(I, p, window_size + 2) 
+        ip_window = numpy.multiply(ip_window, gauss_window)
 
-  for y in range(0, window_size):
-    for x in range(0, window_size):
-      D[y][x] = I[y_p - half_w_size + y][x_p - half_w_size + x]
+        # creates bin for each point
+        for y in range(0, window_size):
+          for x in range(0, window_size):
 
-  return(D)
+            magnitude_p = magnitude(ip_window, [y + 1, x + 1])
+            orientation_p = orientation(ip_window, [y + 1, x + 1])
+            bins[i][0][math.floor(orientation_p /10.0)] += magnitude_p
+            hold[0][math.floor(orientation_p /10.0)] += 1
 
-I = cv2.imread('erimitage.jpg', 0)
-D = create_window(I, [100,100], 3)
+            #print(str([y, x]) + " " + "magnitude: " + str(magnitude_p) \
+            #    + " " + "orientation: " + str(orientation_p))
 
+        i += 1
+  h.points_to_txt(bins, "bins_for_orientation.txt")
+  print(hold)
 
-
-# Test of create_window function
-"""
-print("this is A:\n")
-A = [[I[99][99],  I[99][100],  I[99][101]], \
-     [I[100][99], I[100][100], I[100][101]], \
-     [I[101][99], I[101][100],  I[101][101]]]
-print(numpy.array(A))
-print("end of A \n\n")
-
-print("this is D:")
-print(D)
-print("end of D \n\n")
-
-
-
-points = harris.harris('erimitage.jpg', 0.04, 12000)
-"""
+  
+I = cv2.imread('erimitage2.jpg', 0)
+points = h.txt_to_points('interest_points.txt')
+sift_orientation(I, points, 19)
