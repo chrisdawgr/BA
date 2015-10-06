b88617088d96667cdecacbfaaa1a35cd6f44b1b7
chrisdawgr <christofferthrysoe@hotmail.com>
2015-10-06 14:56:41 +0200

    added magnitude and its pretty

--- SIFT.py
+++ SIFT.py
@@ -9,26 +9,23 @@ import csv
 from PIL import Image
 import helperfunctions as h
 
-def getaverage(points):
-  return (reduce(lambda x, y: x + y, points) / len(points))
-
 def find_max(dog1, dog2, dog3, y, x, princip_cur):
   """
   TODO: Point must be x percent larger than nearest.
   Determines if the given point(y,x) is a maximum or minimum point
   among it's 26 neighbours, in the scale above and below it.
   """
-
+  
   dog1 = h.create_window(dog1, [y, x], 3)
   dog2 = h.create_window(dog2, [y, x], 3)
   dog3 = h.create_window(dog3, [y, x], 3)
   point = dog2[1][1]
-
+  magnitude =  math.sqrt((dog2[1][0] - dog2[1][2])**2 + (dog2[0][1] - dog2[2][1])**2)
   # Create array of neighbouring points 
   dog_points = numpy.array([dog1, dog2, dog3])
   dog_points = dog_points.reshape(-1)            # make 1-dimensional
   dog_points = numpy.delete(dog_points, 13)      # CT: Delete point [1][1]
-
+  
   i = 0
   maxi = 0
   mini = 0
@@ -51,19 +48,16 @@ def find_max(dog1, dog2, dog3, y, x, princip_cur):
       H = numpy.matrix([[Dxx, Dxy, Dxs], [Dxy, Dyy, Dys], [Dxs, Dys, Dss]]) 
       DX = numpy.matrix([[Dx], [Dy], [Ds]])
       det = float(numpy.linalg.det(H))
-
       if (det > 0):
         tr = float(Dxx) + float(Dyy) + float(Dss)
         r = float(princip_cur)
-        #print(tr**2, det)
         xhat = numpy.linalg.inv(H) * DX
         Dxhat = point + (1/2.0) * DX.transpose() * xhat
         #print(str(tr**2 / det) + "<" + str((r+1)**2 / r) + "\t\t\txhat=" + str(abs(Dxhat)))
-
         #print("this is point  \n" + str(point) + "\n\n\n")
         #print("this is DX  \n" + str(DX) + "\n\n\n")
         #print("this is xhat  \n" + str(xhat) + "\n\n\n")
-        if ((tr**2 / det < (r + 1)**2 / r) and (abs(Dxhat)) > 0.03):
+        if ((tr**2 / det < (r + 1)**2 / r) and (abs(Dxhat)) > 0.03) and magnitude > 3.0:
           return 1
         else:
           return 0
@@ -71,16 +65,6 @@ def find_max(dog1, dog2, dog3, y, x, princip_cur):
         return 0
   return 0
 
-
-
-def half_image(I):
-  """
-  Returns an image 1/2 size of the inputted
-  """
-  return cv2.resize(I, (0,0), fx=0.5, fy=0.5)
-
-# NOTE: SIFT calls the "eliminating_edge_responses function, and shows
-# final picture
 def SIFT(filename, r_mag):
   """
   Returns the interest points found
@@ -88,10 +72,10 @@ def SIFT(filename, r_mag):
   s = 3
   k = 2 ** (1.0 / s)
   I = cv2.imread(filename)
-  #I1 = half_image(I)
-  #I2 = half_image(I1)
-  #I3 = half_image(I2)
-  I_bw = cv2.imread(filename, 0)
+  #I1 = misc.imresize(I, 50, 'bilinear')
+  #I2 = misc.imresize(I1, 50, 'bilinear')
+  #I3 = misc.imresize(I2, 50, 'bilinear')
+  #I_bw = cv2.imread(filename, 0)
   dim = I_bw.shape
   height = dim[0]
   length = dim[1]
@@ -105,12 +89,7 @@ def SIFT(filename, r_mag):
   sigma1 = [math.sqrt(0.5), math.sqrt(1), math.sqrt(2), math.sqrt(4),
            math.sqrt(8), math.sqrt(16), math.sqrt(32), math.sqrt(64),
            math.sqrt(128), math.sqrt(256), math.sqrt(512)]
-  #sigma1 = numpy.array([1.3, 1.6, 1.6 * k, 1.6 * (k ** 2), 1.6 * (k ** 3), 1.6 * (k ** 4)])
-
-  #o1sctest = list(numpy.zeros((I.shape[0], I.shape[1], 6)))
-
-  #for i in range(0, 6):
-    #o1sctest[i] = scipy.ndimage.filters.gaussian_filter(I, sigma = sigma1[i])
+  sigma1 = numpy.array([1.3, 1.6, 1.6 * k, 1.6 * (k ** 2), 1.6 * (k ** 3), 1.6 * (k ** 4)])
 
   o1sc = [
   scipy.ndimage.filters.gaussian_filter(I_bw,sigma = sigma1[0]),
@@ -142,120 +121,109 @@ def SIFT(filename, r_mag):
   scipy.ndimage.filters.gaussian_filter(I_bw,sigma = sigma[10])
   ]
   """
-
-  # Append for differnt scales of image
-
+  # Calculate difference of gaussian images.
   DoG_scale1 = []
   for i in range(0, 4):
     DoG_scale1.append(h.matrix_substraction(o1sc[i + 1], o1sc[i]))
-
+  
+  # Consctruct difference of gaussian pyramids
+  # Scale 1:
   dog1 = DoG_scale1[0]
   dog2 = DoG_scale1[1]
   dog3 = DoG_scale1[2]
   dog4 = DoG_scale1[3]
-
-    
+  # Scale 1/2
   """
   dog6 = DoG_scale2[0]
   dog7 = DoG_scale2[1]
   dog8 = DoG_scale2[2]
   dog9 = DoG_scale2[3]
-    
+  #scale 1/4
   dog11 = DoG_scale3[0]
   dog12 = DoG_scale3[1]
   dog13 = DoG_scale3[2]
   dog14 = DoG_scale3[3]
+  # Scale 1/8
   dog16 = DoG_scale4[0]
   dog17 = DoG_scale4[1]
   dog18 = DoG_scale4[2]
   dog19 = DoG_scale4[3]
   """
-   
+  
+  # Initialize arrays for keypoints
   DoG_extrema_points_1_1 = []
   DoG_extrema_points_1_2 = []
-  DoG_extrema_points_2 = []
-  DoG_extrema_points_3 = []
-  DoG_extrema_points_4 = []
+  DoG_extrema_points_2_1 = []
+  DoG_extrema_points_2_2 = []
+  DoG_extrema_points_3_1 = []
+  DoG_extrema_points_3_2 = []
+  DoG_extrema_points_4_1 = []
+  DoG_extrema_points_4_2 = []
    
-  print("Find max")
+  print("Caluclating extrema, performing accurate keypoint localization and edge supression")
+  print("Finding points for scale 1")
   for y in range(3, height - 3):
     for x in range(3, length - 3):
       if (find_max(dog1, dog2, dog3, y, x, r_mag) == 1):
-        I[y][x] = [0,0,255]
         DoG_extrema_points_1_1.append([y,x])
-
-
       if (find_max(dog2, dog3, dog4, y, x, r_mag) == 1):
-        I[y][x] = [0,0,255]
         DoG_extrema_points_1_2.append([y,x])
-  print("end of find max")
-  #cv2.imshow('image', I)
-  #cv2.waitKey(0)
-
-  #cv2.imshow('image', I)
-  #cv2.waitKey(0)
-
   """
+  print "Finding points for scale 2"
   for y in range(3, height1 - 3):
     for x in range(3, length1 - 3):
       if (find_max(dog6, dog7, dog8, y, x) == 1):
-        I1[y][x] = [0,0,255]
-        DoG_extrema_points_2.append([x,y])
+        DoG_extrema_points_2_1.append([x,y])
       if (find_max(dog7, dog8, dog9, y, x) == 1):
-        I1[y][x] = [0,0,255]
-        DoG_extrema_points_2.append([x,y])
+        DoG_extrema_points_2_2.append([x,y])
+
+  print "Finding points for scale 3"
   for y in range(3, height2 - 3):
     for x in range(3, length2 - 3):
       if (find_max(dog11, dog12, dog13, y, x) == 1):
-        I2[y][x] = [0,0,255]
-        DoG_extrema_points_3.append([x,y])
+        DoG_extrema_points_3_1.append([x,y])
       if (find_max(dog12, dog13, dog14, y, x) == 1):
-        I2[y][x] = [0,0,255]
-        DoG_extrema_points_3.append([x,y])
+        DoG_extrema_points_3_2.append([x,y])
+
+  print "Finding points for scale 4"
   for y in range(3, height3 - 3):
     for x in range(3, length3 - 3):
       if (find_max(dog16, dog17, dog18, y, x) == 1):
-        I3[y][x] = [0,0,255]
-        DoG_extrema_points_4.append([x,y])
+        DoG_extrema_points_4_1.append([x,y])
       if (find_max(dog17, dog18, dog19, y, x) == 1):
-        I3[y][x] = [0,0,255]
-        DoG_extrema_points_4.append([x,y])
+        DoG_extrema_points_4_2.append([x,y])
   """
-
-  # Writing points to file "out.txt"
-
-  # cv2.imwrite('erimitage2.jpg',  I)
+  # Scale 1:
   dogn1 =  numpy.array(DoG_extrema_points_1_1)
   dogn2 =  numpy.array(DoG_extrema_points_1_2)
-  result = numpy.vstack([dogn1, dogn2])
-
+  result = set(numpy.vstack([dogn1, dogn2])) # set removes dublicates.
   h.points_to_txt(result, "interest_points.txt", "\n")
+  h.color_pic(I, result, filename[:-4] + "-sift-"+ "r-" + str(r_mag) + ".jpg")
+  
+  # scale 2:
+  #dogn3 =  numpy.array(DoG_extrema_points_2_1)
+  #dogn4 =  numpy.array(DoG_extrema_points_2_2)
+  #result = set(numpy.vstack([dogn3, dogn4])) # set removes dublicates.
+  #h.points_to_txt(result, "interest_points_2.txt", "\n")
+  #h.color_pic(I, result, filename[:-4] + "-sift_2-"+ "r-" + str(r_mag) + ".jpg")
 
-  color_pic(I, result, filename[:-4] + "-sift-"+ "r-" + str(r_mag) + ".jpg")
+  # scale 3:
+  #dogn5 =  numpy.array(DoG_extrema_points_3_1)
+  #dogn6 =  numpy.array(DoG_extrema_points_3_2)
+  #result = set(numpy.vstack([dogn5, dogn6])) # set removes dublicates.
+  #h.points_to_txt(result, "interest_points_3.txt", "\n")
+  #h.color_pic(I, result, filename[:-4] + "-sift_3-"+ "r-" + str(r_mag) + ".jpg")
 
-# input(I, points, name) - points are a list of [y, x] vals, name is optional,
-# but should be a string
-def color_pic(*arg):
-  I = arg[0]
-  points = arg[1]
+  # scale 4:
+  #dogn7 =  numpy.array(DoG_extrema_points_4_1)
+  #dogn8 =  numpy.array(DoG_extrema_points_4_2)
+  #result = set(numpy.vstack([dogn7, dogn8])) # set removes dublicates.
+  #h.points_to_txt(result, "interest_points_2.txt", "\n")
+  #h.color_pic(I, result, filename[:-4] + "-sift_4-"+ "r-" + str(r_mag) + ".jpg")
 
-  if (len(arg) >= 2):
-    for p in points:
-      I[p[0]][p[1]] = [0,0,255]
-
-  if (len(arg) == 3):
-    name = arg[2]
-    cv2.imwrite(name, I)
-
-
-#currently assuming, a window size of 3x3
-#NOT working properly, and NOT used in SIFT function
 def test_SIFT(filename, r, increment, iterations):
   for i in range(0, iterations):
     print(filename, r + (i * increment))
     SIFT(filename, r + (i * increment))
 
-
 test_SIFT('erimitage2.jpg', 0.1, 0.1, 10)
-
-#SIFT('erimitage.jpg', 10)
