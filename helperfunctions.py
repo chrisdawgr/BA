d4eaa9533ce2d20b1fb1e08712709134268d44ee
slagterfranz <malogest@gmail.com>
2015-10-23 19:39:18 +0200

    test suite up and misc things updated

--- helperfunctions.py
+++ helperfunctions.py
@@ -37,7 +37,7 @@ def gaussdx(size, sigma):
   for y in range(0, size):
     x1 = -4.5
     for x in range(0, size):
-      gauss_calc = (-((x1/(2.0*math.pi*sigma**4.0)*(math.exp(-((x1**2.0+y1**2.0)/(2.0*sigma**2.0)))))))
+      gauss_calc = (((-x1/(2.0*math.pi*sigma**4.0)*(math.exp(-((x1**2.0+y1**2.0)/(2.0*sigma**2.0)))))))
       gauss_kernel[y][x] = gauss_calc
       x1 += stepsize
     y1 += stepsize
@@ -221,3 +221,107 @@ def color_pic(*arg):
   if (len(arg) == 3):
     name = arg[2]
     cv2.imwrite(name, I)
+
+
+
+def drawMatches(I1, kp1, I2, kp2, matches):
+  """
+  img1,img2 - Grayscale images
+  kp1,kp2 - Detected list of keypoints through any of the OpenCV keypoint 
+            detection algorithms
+  matches - A list of matches of corresponding keypoints through any
+            OpenCV keypoint matching algorithm
+  """
+  img1 = cv2.imread(I1, 0)
+  img2 = cv2.imread(I2, 0)
+
+  # Create a new output image that concatenates the two images together
+  # (a.k.a) a montage
+  rows1 = len(img1)
+  cols1 = len(img1[0])
+  rows2 = len(img2)
+  cols2 = len(img2[0])
+
+  out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
+
+  # Place the first image to the left
+  out[:rows1,:cols1] = np.dstack([img1, img1, img1])
+
+  # Place the next image to the right of it
+  out[:rows2,cols1:] = np.dstack([img2, img2, img2])
+
+  # For each pair of points we have between both images
+  # draw circles, then connect a line between them
+  for mat in range(0, len(kp1)):
+
+    # Get the matching keypoints for each of the images
+    # x - columns
+    # y - rows
+    (x1,y1) = (kp1[mat])
+    (x2,y2) = (kp2[mat])
+
+    # Draw a small circle at both co-ordinates
+    # radius 4
+    # colour blue
+    # thickness = 1
+    cv2.circle(out, (int(y1),int(x1)), 4, (255, 0, 0), 1)   
+    cv2.circle(out, (int(y2)+cols1,int(x2)), 4, (255, 0, 0), 1)
+
+    # Draw a line in between the two points
+    # thickness = 1
+    # colour blue
+    cv2.line(out, (int(y1),int(x1)), (int(y2)+cols1,int(x2)), (255, 0, 0), 1)
+
+
+  # Show the image
+  cv2.imshow('Matched Features', out)
+  cv2.waitKey(0)
+  cv2.destroyWindow('Matched Features')
+
+  # Also return the image if you'd like a copy
+  return out
+
+def oneNN(descss1, descss2, pp1, pp2):
+  print(len(descss1), len(pp1))
+  print(len(descss2), len(pp2))
+  res_1 = []
+  res_2 = []
+  p1 = []
+  p2 = []
+  descs1 = []
+  descs1 = []
+  ret_flag = 0
+
+  if(len(pp1) < len(pp2)):
+    p1 = pp1
+    p2 = pp2
+    descs1 = descss1
+    descs2 = descss2
+
+  else:
+    p1 = pp2
+    p2 = pp1
+    descs1 = descss2
+    descs2 = descss1
+    ret_flag = 1
+
+  for i_desc1 in range(0, len(descs1)):
+    desc1 = np.matrix(descs1[i_desc1])
+    s_dist = float("inf")
+    s_dist_index2 = 0
+    for i_desc2 in range(0, len(descs2)):
+      desc2 = np.matrix(descs2[i_desc2])
+      dist = np.linalg.norm(desc1 - desc2)
+
+      if (dist < s_dist):
+        s_dist = dist
+        s_dist_index2 = i_desc2
+
+    res_1.append(p1[i_desc1])
+    res_2.append(p2[s_dist_index2])
+
+  if(ret_flag == 0):
+    return(res_1, res_2)
+  else:
+    print("heer")
+    return(res_2, res_1)
